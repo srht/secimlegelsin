ef974d26495e4820185e10b6fa2adb95e663f1df
Serhat <srhtkrgl@gmail.com>
2023-12-07 23:36:39 +0300

    adasad

---
+++ src/data/sehirler.ts
@@ -0,0 +1,17 @@
+interface sehir {
+    name:string;
+    slug:string;
+}
+
+const sehirler:sehir[] = [{name:"İstanbul", slug:"istanbul"},
+{name:"Ankara", slug:"ankara"},
+{name:"İzmir", slug:"izmir"},
+{name:"Bursa", slug:"bursa"},
+{name:"Adana", slug:"adana"},
+{name:"Antalya", slug:"antalya"},
+{name:"Konya", slug:"konya"},
+{name:"Gaziantep", slug:"gaziantep"},
+{name:"Mersin", slug:"mersin"},
+{name:"Diyarbakır", slug:"diyarbakir"}];
+
+export default sehirler;
