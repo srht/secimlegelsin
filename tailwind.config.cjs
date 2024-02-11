6e68c393aafa6930ceada57a1c0b836f5ac69088
Serhat <srhtkrgl@gmail.com>
2024-02-11 22:26:27 +0300

    talep sehir

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,6 +17,7 @@ module.exports = {
       transparent: colors.transparent,
       primary: "#EA3333",//"#9333EA",
       secondary: "#ff7e33",
+      btntalep:"#0ea5e9",
       info: "#0C63E7",
       black: colors.black,
       white: colors.white,
