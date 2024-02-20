f203173ab97ba6b619c890c353aa5470f16c188e
Serhat Köroğlu <srhtkrgl@outlook.com>
2024-02-20 16:53:17 +0300

    cities esnson

--- src/components/Alkis.jsx
+++ src/components/Alkis.jsx
@@ -1,38 +1,50 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from "@supabase/ssr";
 import { supabase } from "../lib/supabase";
-import { useEffect, useState } from 'react';
-export default function Alkis(param){
-    const [totalVotes, setTotalVote]=useState(0)
-    useEffect(()=>{
-       getVoteCount()
-    },[])
+import { useEffect, useState } from "react";
+export default function Alkis(param) {
+  const [totalVotes, setTotalVote] = useState(0);
+  useEffect(() => {}, []);
 
-   async function getVoteCount(){
-       const {data,count} =await supabase.from('votes')
-        .select('id', { count: 'exact'})
-        .eq('requestid',param.requestid);
-        console.log('data',data)
-        setTotalVote(count)
-    }
+  async function getVoteCount() {
+    const { data, count } = await supabase
+      .from("votes")
+      .select("id", { count: "exact" })
+      .eq("requestid", param.requestid);
+    console.log("data", data);
+    setTotalVote(count);
+    document.getElementById("vr-" + param.requestid).innerHTML = count;
+  }
 
-    const alkisClick=async ()=>{
-console.log(param)
-console.log(param.user)
-supabase.from('votes').insert({
-    selection:'afasd',
-    userid:param.user.id,
-    requestid:param.requestid
-}).then((res)=>{
-    setTotalVote(totalVotes+1)
-})
-        
-    }
+  const alkisClick = async () => {
+    getVoteCount();
+    console.log(param);
+    console.log(param.user);
+    supabase
+      .from("votes")
+      .insert({
+        selection: "afasd",
+        userid: param.user.id,
+        requestid: param.requestid,
+      })
+      .then((res) => {
+        setTotalVote(totalVotes + 1);
+        document.getElementById("vr-" + param.requestid).innerHTML =
+          totalVotes + 1;
+      });
+  };
 
-    const alkisButton=param.user?<div><button onClick={alkisClick} id="alkis"><img class="w-10" src='/images/clapping.png' /></button><br/><strong class="text-lg">{totalVotes}</strong></div>:
-    <a class="transition duration-500 group-hover:scale-105" href="/login"><img class="w-10" src="/images/clapping.png" /></a>;
-  
-    return (<div>{alkisButton}</div>)
+  const alkisButton = param.user ? (
+    <div>
+      <button onClick={alkisClick} id="alkis">
+        <img class="w-10" src="/images/clapping.png" />
+      </button>
+      <br />
+    </div>
+  ) : (
+    <a class="transition duration-500 group-hover:scale-105" href="/login">
+      <img class="w-10" src="/images/clapping.png" />
+    </a>
+  );
+
+  return <div>{alkisButton}</div>;
 }
-
-
-
