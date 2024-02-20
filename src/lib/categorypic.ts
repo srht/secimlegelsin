23586ab95730f3a29f1ebe21c0c27b1c068bb46e
Serhat <srhtkrgl@gmail.com>
2024-02-20 23:20:27 +0300

    talep

--- src/lib/categorypic.ts
+++ src/lib/categorypic.ts
@@ -1,5 +1,5 @@
 import { talepturleri } from "../data/tumdata";
 export default function CategoryPic(tur:string){
    let foundTur= talepturleri.find(t=>t.name===tur);
-   return foundTur.slug;
+   return foundTur?.slug;
 }
