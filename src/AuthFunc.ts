113209caa23668c5988ad352f1761af87d86d37f
Serhat Köroğlu <srhtkrgl@outlook.com>
2024-02-23 09:47:13 +0300

    yeni talep login

--- src/AuthFunc.ts
+++ src/AuthFunc.ts
@@ -4,14 +4,16 @@ import { supabase } from './lib/supabase';
 export async function getUserAuth(context: APIContext) {
   const { cookies, redirect } = context;
   const accessToken = cookies.get("sb-access-token");
-      const refreshToken = cookies.get("sb-refresh-token");
-      if (!accessToken || !refreshToken) {
-        return context.redirect("/login");
-      }
-      
-      const { data, error } = await supabase.auth.setSession({
-        refresh_token: refreshToken.value,
-        access_token: accessToken.value,
-      });
-    return data;
+  const refreshToken = cookies.get("sb-refresh-token");
+  console.log(accessToken)
+  console.log(refreshToken)
+  if (!accessToken || !refreshToken) {
+    return context.redirect("/login");
+  }
+
+  const { data, error } = await supabase.auth.setSession({
+    refresh_token: refreshToken.value,
+    access_token: accessToken.value,
+  });
+  return data;
 }
