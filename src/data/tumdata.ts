aa1757a04e9ff1f4385d6628187257c50c2f81cc
Serhat Köroğlu <srhtkrgl@outlook.com>
2024-02-27 11:20:44 +0300

    show categories

--- src/data/tumdata.ts
+++ src/data/tumdata.ts
@@ -1,34 +1,41 @@
 interface sehir {
-    name:string;
-    slug:string;
+    name: string;
+    slug: string;
 }
 
 interface taleptur {
-    name:string;
-    slug:string;
+    name: string;
+    slug: string;
 }
 
-const talepturleri:taleptur[] = [
-    {name:"Eğitim", slug:"egitim"},
-    {name:"Yollar-Caddeler", slug:"yollar"},
-    {name:"Mahalleler", slug:"mahalleler"},
-    {name:"Toplu Taşıma", slug:"tasima"}
+const talepturleri: taleptur[] = [
+    { name: "Eğitim", slug: "egitim" },
+    { name: "Yollar-Caddeler", slug: "yollar" },
+    { name: "Mahalleler", slug: "mahalleler" },
+    { name: "Toplu Taşıma", slug: "tasima" }
 ];
 
-const sehirler:sehir[] = [{name:"İstanbul", slug:"istanbul"},
-{name:"Ankara", slug:"ankara"},
-{name:"İzmir", slug:"izmir"},
-{name:"Bursa", slug:"bursa"},
-{name:"Adana", slug:"adana"},
-{name:"Antalya", slug:"antalya"},
-{name:"Konya", slug:"konya"},
-{name:"Gaziantep", slug:"gaziantep"},
-{name:"Mersin", slug:"mersin"},
-{name:"Kocaeli", slug:"kocaeli"},
-{name:"Trabzon", slug:"trabzon"},
-{name:"Eskişehir", slug:"eskisehir"},
-{name:"Erzurum", slug:"erzurum"},
-{name:"Samsun", slug:"samsun"}
+const sehirler: sehir[] = [{ name: "İstanbul", slug: "istanbul" },
+{ name: "Ankara", slug: "ankara" },
+{ name: "İzmir", slug: "izmir" },
+{ name: "Bursa", slug: "bursa" },
+{ name: "Adana", slug: "adana" },
+{ name: "Antalya", slug: "antalya" },
+{ name: "Konya", slug: "konya" },
+{ name: "Gaziantep", slug: "gaziantep" },
+{ name: "Mersin", slug: "mersin" },
+{ name: "Kocaeli", slug: "kocaeli" },
+{ name: "Trabzon", slug: "trabzon" },
+{ name: "Eskişehir", slug: "eskisehir" },
+{ name: "Erzurum", slug: "erzurum" },
+{ name: "Samsun", slug: "samsun" }
 ];
 
-export {sehirler, talepturleri};
+function getCategory(slug: string) {
+    let found = talepturleri.find(tur => tur.slug == slug);
+    console.log(slug)
+    console.log(found)
+    return found?.name;
+}
+
+export { sehirler, talepturleri, getCategory };
