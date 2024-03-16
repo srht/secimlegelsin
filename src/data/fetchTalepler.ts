a1708f51268a2461d7cd6fde64ab441edcd764a3
Serhat <srhtkrgl@gmail.com>
2024-03-17 01:56:14 +0300

    appfoot change

--- src/data/fetchTalepler.ts
+++ src/data/fetchTalepler.ts
@@ -2,13 +2,14 @@ import { supabase } from "../lib/supabase"
 
 export async function FetchTalepler(recent, city) {
 console.log(city)
+console.log(recent)
     if (recent) {
-        const { data, error } = await supabase.from("retrieveview").select().order("created_at", { ascending: false }).limit(50);
+        const { data, error } = await supabase.from("retrieveview").select().order("created_at", { ascending: false }).limit(150);
         return data
     }
     else {
         const { data, error } = city ? await supabase.from("retrieveview").select().order("count", { ascending: false }).eq('address', city.toLocaleUpperCase('tr')[0] + city.substring(1)).limit(50)
-            : await supabase.from("retrieveview").select().order("count", { ascending: false }).limit(50)
+            : await supabase.from("retrieveview").select().order("count", { ascending: false }).limit(150)
         //const {data,error}=await supabase.from('retrieveview').select()
         return data;
     }
