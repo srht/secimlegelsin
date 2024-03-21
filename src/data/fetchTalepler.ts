c70b08c4779b62627c10f208cfb1661d59c1a828
Serhat <srhtkrgl@gmail.com>
2024-03-21 23:59:57 +0300

    latest category

--- src/data/fetchTalepler.ts
+++ src/data/fetchTalepler.ts
@@ -1,11 +1,20 @@
 import { supabase } from "../lib/supabase"
+import { getCategory } from "./tumdata";
 
-export async function FetchTalepler(recent, city) {
+export async function FetchTalepler(recent, city, category) {
 console.log(city)
 console.log(recent)
+
     if (recent) {
         const { data, error } = await supabase.from("retrieveview").select().order("created_at", { ascending: false }).limit(150);
         return data
+    }
+    else if(category){
+        let nameCat=getCategory(category)
+        const { data, error } = await supabase.from("retrieveview").select().order("count", { ascending: false }).eq('category', nameCat).limit(50)
+
+    //const {data,error}=await supabase.from('retrieveview').select()
+    return data;
     }
     else {
         const { data, error } = city ? await supabase.from("retrieveview").select().order("count", { ascending: false }).eq('address', city.toLocaleUpperCase('tr')[0] + city.substring(1)).limit(50)
